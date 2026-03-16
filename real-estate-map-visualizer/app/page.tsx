e4de85222aafef1902d9946869696ecb27c55f99
adam_solomon_home <adamsolomon1801@gmail.com>
2026-03-15 20:54:37 -0400

    Update page.tsx

--- real-estate-map-visualizer/app/page.tsx
+++ real-estate-map-visualizer/app/page.tsx
@@ -5,6 +5,7 @@ import dynamic from "next/dynamic";
 const Map = dynamic(() => import("./components/Map"), { ssr: false });
 
 export default function Home() {
+  //re commit for redeploy
   return (
     // app/page.tsx
 
